395a463401ce2b43f91b947915977178a92e9a6d
Igor Polishchuk <amadare42@gmail.com>
2015-11-17 13:58:38 +0200

    Version 0.21

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 ::change version for new releases
-set version=0.2
+set version=0.21
 ::specify path to compiler
 set CompilerFolderPath=%ProgramFiles%\AutoHotkey\Compiler
 
@@ -24,30 +24,22 @@ IF not exist bin (
 
 echo Build SC version...
 %Ahk2ExePath% /in presser.ahk /out bin/presser_SC_v%version%.exe /icon icon.ico /bin %AutoHotkeySCPath%
-if not exist bin/presser_SC_v%version%.exe (
-	echo compiling error!
-	exit
-) 
+if not exist bin/presser_SC_v%version%.exe goto :Error
 
 echo Build Unicode 64 version...
 %Ahk2ExePath% /in presser.ahk /out bin/presser_U64_v%version%.exe /icon icon.ico /bin %AutoHotkeyU64Path%
-if not exist bin/presser_U64_v%version%.exe (
-	echo compiling error!
-	exit
-)
+if not exist bin/presser_U64_v%version%.exe goto :Error
 
 echo Build Unicode 32 version...
 %Ahk2ExePath% /in presser.ahk /out bin/presser_U32_v%version%.exe /icon icon.ico /bin %AutoHotkeyU32Path%
-if not exist bin/presser_U32_v%version%.exe (
-	echo compiling error!
-	exit
-)
+if not exist bin/presser_U32_v%version%.exe goto :Error
 
 echo Build ANSI 32 version...
 %Ahk2ExePath% /in presser.ahk /out bin/presser_ANSI32_v%version%.exe /icon icon.ico /bin %AutoHotkeyANSI32Path%
-if not exist bin/presser_ANSI32_v%version%.exe (
-	echo compiling error!
-	exit	
-)
+if not exist bin/presser_ANSI32_v%version%.exe goto :Error
 
 echo Done.
+exit
+:Error
+echo compiling error!
+exit	
